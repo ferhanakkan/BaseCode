551fc2301f1a903fb3ab0bdc39e459d047ba0e06
Ferhan Akkan <ferhanakkan@gmail.com>
2020-07-30 10:31:18 +0300

    Initial Commit

---
+++ BaseCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseCode
+//
+//  Created by Ferhan Akkan on 30.07.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
